8196927e90236249aa61eacee47251658e28b4ea
jaypool14 <63919464+jaypool14@users.noreply.github.com>
2020-04-29 21:10:02 +0530

    LOGIN PAGE

---
+++ PROJECT COMP SCI/src/project/comp/sci/Login.java
@@ -0,0 +1,87 @@
+
+package project.comp.sci;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JFrame implements ActionListener {
+
+    JPanel panel;
+    JLabel user_label, password_label, message;
+    JTextField userName_text;
+    JPasswordField password_text;
+    JButton submit, cancel;
+
+   Login() {
+        
+        // User Label
+        user_label = new JLabel();
+        user_label.setText("User Name :");
+        user_label.setFont(new Font("Serif",Font.PLAIN,40));
+        userName_text = new JTextField();
+        
+        // Password
+
+        password_label = new JLabel();
+        password_label.setText("Password :");
+        password_text = new JPasswordField();
+
+        // Submit
+
+        submit = new JButton("SUBMIT");
+
+        panel = new JPanel(new GridLayout(1,4));
+
+        panel.add(user_label);
+        panel.add(userName_text);
+        panel.add(password_label);
+        panel.add(password_text);
+
+        message = new JLabel();
+        panel.add(message);
+        panel.add(submit);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Adding the listeners to components..
+        submit.addActionListener(this);
+        add(panel, BorderLayout.CENTER);
+        setTitle("Please Login Here !");
+        setSize(1500, 750);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Login();
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String userName = userName_text.getText();
+        String password = password_text.getText();
+        if (userName.trim().equals("admin") && password.trim().equals("admin")) {
+            message.setText(" Hello " + userName
+                    + "");
+        } else {
+            message.setText(" Invalid user.. ");
+        }
+
+    }
+
+}
+    
+    
+
---
+++ PROJECT COMP-SCI/LOGIN_MENU.java
@@ -0,0 +1,6 @@
+
+
+public class LOGIN_MENU
+{
+   
+}
