ab450f5a186cce092005911aff9bca3dc4bf1eab
jaypool14 <63919464+jaypool14@users.noreply.github.com>
2020-05-12 20:57:53 +0530

    SQL

---
+++ PROJECT COMP-SCI/SQLiteJDBC.java
@@ -0,0 +1,50 @@
+import java.sql.*;
+
+ 
+
+public class SQLiteJDBC {
+
+ 
+
+  public static void main( String args[] ) {
+
+ 
+
+   Connection c = null;
+   Statement stmt = null;
+   try {
+      //Class.forName("org.sqlite.JDBC");
+      
+      c = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\user\\Documents\\PROJECT COMP SCI\\LAUNDRY-SYSTEM\\PROJECT COMP-SCI\\PRACTICE.db");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+ 
+
+      stmt = c.createStatement();
+      ResultSet rs = stmt.executeQuery( "SELECT * FROM login;" );
+      
+      while ( rs.next() ) {
+         String  username = rs.getString("username");
+   
+         String  password = rs.getString("password");
+         
+         
+         
+         System.out.println( username );
+
+ 
+
+         System.out.println(password);
+         System.out.println();
+      }
+      rs.close();
+      stmt.close();
+      c.close();
+   } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+   }
+   System.out.println("Operation done successfully");
+  }
+}
