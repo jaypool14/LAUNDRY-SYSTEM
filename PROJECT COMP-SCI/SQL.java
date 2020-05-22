d84767fb0361fa4083e95d03173d9c242004a072
Febin Jose <joenibe@gmail.com>
2020-05-22 20:08:25 +0530

    sql

--- PROJECT COMP-SCI/SQL.java
+++ PROJECT COMP-SCI/SQL.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 
 public class SQL {
-    public static void execute(String query, int ret)
+    public static ResultSet execute(String query)
     {
        Connection c = null;
        Statement stmt = null;
@@ -13,11 +13,7 @@ public class SQL {
        c.setAutoCommit(false);
        System.out.println("Opened database successfully");
        stmt = c.createStatement();
-       if (ret==0)
        rs = stmt.executeQuery(query);
-       else
-       stmt.executeQuery(query);
-      
        /*while ( rs.next() ) {
          String  username = rs.getString("username");
          String  password = rs.getString("password");  
@@ -29,20 +25,56 @@ public class SQL {
        //stmt.close();
        //c.close();
        //if (ret==0)
-       //return rs;
+       return rs;
        //else
        //return null;
        } 
        catch ( Exception e ) {
        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
        System.exit(0);
+       return null;
+       }
+    }
+    
+    public static ResultSet updatequery(String query)
+    {
+       Connection c = null;
+       Statement stmt = null;
+       ResultSet rs = null;
+       try {
+        //Class.forName("org.sqlite.JDBC");
+      
+       c = DriverManager.getConnection("jdbc:sqlite:.\\PRACTICE.db");
+       c.setAutoCommit(false);
+       System.out.println("Opened database successfully");
+       stmt = c.createStatement();
+       rs = stmt.executeQuery(query);
+       /*while ( rs.next() ) {
+         String  username = rs.getString("username");
+         String  password = rs.getString("password");  
+         System.out.println( username );
+         System.out.println(password);
+         System.out.println();
+       }*/
+       
+       //stmt.close();
+       //c.close();
+       //if (ret==0)
+       return rs;
+       //else
        //return null;
+       } 
+       catch ( Exception e ) {
+       System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+       System.exit(0);
+       return null;
        }
     }
 
+
     public static void main( String args[] ) {
        
-       execute("INSERT INTO customer (name,number, address,email) VALUES ('s','22','s3','s4')", 1    );
+       execute("SELECT * FROM login WHERE username='admin' AND password='pass'");
        // System.out.println(customeresult);
       
     }
